20c04bb573fab112754ec3f7c8db719bd44b4cb0
testtesttesttest45 <weilinquek201@gmail.com>
2025-02-15 17:13:14 +0800

    app icons and app name update

--- flutter_app/README.md
+++ flutter_app/README.md
@@ -1,4 +1,4 @@
-# flutter_app
+# Unknown Bridge
 
 A new Flutter project.
 
--- flutter_app/android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ flutter_app/android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_app
+package com.example.unknown_bridge
 
 import io.flutter.embedding.android.FlutterActivity
 
